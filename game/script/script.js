1dd4850c2c9c571b70d9c34f776a0c8463571abd
Hossein <hossein.aghili.2658@gmail.com>
2025-09-16 17:43:35 +0330

    Writing the gameloop function and writing the motion of the directions

--- game/script/script.js
+++ game/script/script.js
@@ -37,11 +37,36 @@ const gameOver = () => {
 const draw = () => {
     ctx.clearRect(0, 0, canvasWidth, canvasHeigtht)
     for (cell of snake) {
-        ctx.fillStyle('green')
+        ctx.fillStyle = 'green'
         ctx.fillRect(cell.x * gridSize, cell.y * gridSize, gridSize - 1, gridSize - 1)
     }
     if (food) {
-        ctx.fillStyle('red')
+        ctx.fillStyle = 'red'
         ctx.fillRect(food.x * gridSize, food.y * gridSize, gridSize - 1, gridSize - 1)
     }
 }
+const gameLoop = () => {
+    draw()
+}
+
+document.addEventListener('keydown', (e) => {
+    switch (code.e) {
+        case "ArrowUp":
+            if (dirction.y === 1) return
+            dirction = { x: 0, y: -1 }
+            break
+        case "ArrowDown":
+            if (dirction.y === -1) return
+            dirction = { x: 0, y: 1 }
+            break
+        case "ArrowLeft":
+            if (dirction.x === 1) return
+            dirction = { x: -1, y: 0 }
+            break
+        case "ArrowRight":
+            if (dirction.x === -1) return
+            dirction = { x: 1, y: 0 }
+    }
+})
+
+gameInterVarl = setInterval(gameLoop,speed)
